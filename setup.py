6696d93243223aa2a14cb59baf073f69f9d3c890
Juan <juanmb@twave.io>
2022-10-28 10:50:30 +0200

    Fix dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="twave-api-client",
-    version="0.1.2",
+    version="0.1.3",
     description="TWave API Client",
     long_description=long_description,
     long_description_content_type="text/markdown",
